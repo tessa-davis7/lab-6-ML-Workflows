608bf086abe7c71070646ff2d1c53a76a629c896
Tessa <tessa7@colostate.edu>
2025-05-09 14:38:01 -0600

    commit pls

---
+++ docs/untitled.1.R
@@ -0,0 +1,13 @@
+project:
+  title:"csu-ess-lab6"
+  output-dir: docs
+  type: website
+format:
+  html:
+    self-contained: TRUE
+
+##Questions
+ # 1: What does zero_q_freq represent?
+  # The frequency of days wit zero percipitation measured in mm/day
+ # 2: Make 2 maps of the sites by aridty and p_mean
+ # 3: Build a xgboost (engine) regression (mode) model using boost_tree Build a neural network model using the nnet engine from the baguette package using the bag_mlp function Add this to the above workflow Evaluate the model and compare it to the linear and random forest models Which of the 4 models would you move forward with?
